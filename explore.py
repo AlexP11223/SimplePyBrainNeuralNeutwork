b50ec7d6dcb2fbfc532eaff590a190ef417f270d
Alex P <alex.pantec@gmail.com>
2018-09-23 12:04:49 +0300

    explore data

---
+++ explore.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from matplotlib import pyplot
+import seaborn as sns
+
+file_path = 'data/data_banknote_authentication.csv'
+attributes = ['varianceWT', 'skewnessWT', 'curtosisWT', 'entropy']
+label = 'class'
+
+df = pd.DataFrame(pd.read_csv(file_path, sep=','))
+
+print(df.describe())
+
+sns.set_palette(sns.xkcd_palette(['pale red', 'medium green']))
+
+sns.countplot(x=label, data=df)
+pyplot.show()
+
+sns.pairplot(data=df, hue=label)
+pyplot.show()
