d8d4d6d20ef3a4d2b4a9492c5b5e7734c1004081
Alex P <alex.pantec@gmail.com>
2018-09-28 16:30:22 +0300

    keep train/validation data in one file

--- norm.py
+++ norm.py
@@ -7,7 +7,6 @@ base_file_name = 'data_banknote_authentication'
 label = 'class'
 
 train_data = pd.read_csv(input_dir + 'train_' + base_file_name + '.csv', sep=',')
-validation_data = pd.read_csv(input_dir + 'validation_' + base_file_name + '.csv', sep=',')
 test_data = pd.read_csv(input_dir + 'test_' + base_file_name + '.csv', sep=',')
 
 scaler = StandardScaler()
@@ -23,9 +22,7 @@ def scale(df):
 
 
 train_data = scale(train_data)
-validation_data = scale(validation_data)
 test_data = scale(test_data)
 
 train_data.to_csv(output_dir + 'train_' + base_file_name + '.csv', sep=',', index=False)
-validation_data.to_csv(output_dir + 'validation_' + base_file_name + '.csv', sep=',', index=False)
 test_data.to_csv(output_dir + 'test_' + base_file_name + '.csv', sep=',', index=False)
